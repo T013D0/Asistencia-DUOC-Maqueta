c31ad9d2b2711e6f07b6be2ea9f990f75323207d
Toledo <josetoledoo6421@gmail.com>
2024-09-26 22:28:16 -0300

    wip

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,15 @@
 // tab1.page.ts
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TabsPage } from '../tabs/tabs.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-
-  constructor(private tabsPage: TabsPage) {}
+  constructor(private tabsPage: TabsPage, private router: Router) {}
 
   ngOnInit() {
     this.tabsPage.hideTabBar = true;
@@ -17,5 +17,12 @@ export class Tab1Page implements OnInit {
 
   ionViewWillLeave() {
     this.tabsPage.hideTabBar = false;
+  }
+
+  alertButtons = ['Aceptar'];
+
+  //Redireccion a la pagina despues de aceptar el alert
+  async presentAlert() {
+    this.router.navigate(['/tabs/tab2']);
   }
 }
