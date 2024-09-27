9f1fdc734aaaec943e3f090701e7ad087a34700d
Toledo <josetoledoo6421@gmail.com>
2024-09-27 08:40:31 -0300

    loginForm

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -2,6 +2,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TabsPage } from '../tabs/tabs.page';
+import { User } from '../../const/types';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +11,16 @@ import { TabsPage } from '../tabs/tabs.page';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page implements OnInit {
-  constructor(private tabsPage: TabsPage, private router: Router) {}
+  usr: User = {
+    email: '',
+    password: '',
+  };
+
+  constructor(
+    private tabsPage: TabsPage,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
     this.tabsPage.hideTabBar = true;
@@ -22,7 +33,40 @@ export class Tab1Page implements OnInit {
   alertButtons = ['Aceptar'];
 
   //Redireccion a la pagina despues de aceptar el alert
-  async presentAlert() {
-    this.router.navigate(['/tabs/tab2']);
+  async onSubmit() {
+    const email = this.usr.email;
+
+    const isStudent =
+      email.includes('@duocuc.cl') || email.includes('@duoc.cl');
+    const isTeacher =
+      email.includes('@profesor.duocuc.cl') ||
+      email.includes('@profesor.duoc.cl');
+
+    if (isStudent) {
+      this.router.navigate(['/tabs/tab3']);
+      const alert = await this.alertController.create({
+        header: 'Sesion iniciada',
+        message: 'Bienvenido a duocUC',
+        buttons: this.alertButtons,
+      });
+      await alert.present();
+    } else if (isTeacher) {
+      this.router.navigate(['/tabs/tab2']);
+      const alert = await this.alertController.create({
+        header: 'Sesion iniciada',
+        message: 'Bienvenido a duocUC',
+        buttons: this.alertButtons,
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'El correo ingresado no es valido',
+        buttons: this.alertButtons,
+      });
+      await alert.present();
+    }
+
+    //this.router.navigate(['/tabs/tab2']);
   }
 }
