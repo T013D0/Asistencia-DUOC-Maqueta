bbba230e4589eab6e6baf0b5835a293208d251e7
Qvverty72 <car.sotoa@duocuc.cl>
2024-11-06 18:23:17 -0300

    added firebase database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicStorageModule } from '@ionic/storage-angular';
 
+import { environment } from '../environments/environment';
+import{AngularFireAuthModule} from "@angular/fire/compat/auth";
+import{AngularFireModule} from "@angular/fire/compat";
+import{AngularFirestoreModule} from "@angular/fire/compat/firestore";
+
+
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(),AngularFireModule.initializeApp(environment.firebaseConfig)
+    ,AngularFireAuthModule,AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: { // Configuración de Firebase  
+    apiKey: "AIzaSyA37XqTCIAVmGkk9SgyV-yIwKfRbtbQikY",
+    authDomain: "asistenciaduoc-8b09b.firebaseapp.com",
+    projectId: "asistenciaduoc-8b09b",
+    storageBucket: "asistenciaduoc-8b09b.firebasestorage.app",
+    messagingSenderId: "791051020497",
+    appId: "1:791051020497:web:c015b11ed38b1ad7617df0",
+    measurementId: "G-R5FZPMQT45"
+  }
 };
 
 /*
