eb65682efc8880475acbb16aec7da2cea9ac8170
Qvverty72 <car.sotoa@duocuc.cl>
2024-11-21 18:29:19 -0300

    added icons and modified the capacitor config.ts for adding the custom name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Asistencia',
+  appId: 'Asistencia DuocUC',
+  appName: 'Asistencia DuocUC',
   webDir: 'www'
 };
 
