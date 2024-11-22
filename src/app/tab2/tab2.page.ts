5e22c1c252dcc65b36cf548e189b29c59f8adfac
Toledo <josetoledoo6421@gmail.com>
2024-11-22 18:31:46 -0300

    username

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,21 +8,27 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  name: any;
+  last_name: any;
   constructor(
     private supabaseauthService: SupabaseauthService,
     private alertController: AlertController
   ) {}
 
-
+  ngOnInit() {
+    this.supabaseauthService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        this.name = user.user_metadata['name'];
+        this.last_name = user.user_metadata['last_name'];
+      }
+    });
+  }
 
   async signout() {
     await this.supabaseauthService.signOut();
-    await this.showAlert(
-      "Salida exitosa" ,"¡Hasta luego!",
-    );
+    await this.showAlert('Salida exitosa', '¡Hasta luego!');
     window.location.href = 'tabs/tab1';
   }
-
 
   async showAlert(header: string, message: string) {
     const alert = await this.alertController.create({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,37 +5,37 @@ import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-
+  name: any;
+  last_name: any;
   constructor(
     private supabaseauthService: SupabaseauthService,
     private alertController: AlertController
-  ) {
+  ) {}
 
-
-
-
-    
+  ngOnInit() {
+    this.supabaseauthService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        this.name = user.user_metadata['name'];
+        this.last_name = user.user_metadata['last_name'];
+      }
+    });
   }
 
-    async signout() {
-      await this.supabaseauthService.signOut();
-      await this.showAlert(
-        "Salida exitosa" ,"¡Hasta luego!",
-      );
-      window.location.href = 'tabs/tab1';
-    }
+  async signout() {
+    await this.supabaseauthService.signOut();
+    await this.showAlert('Salida exitosa', '¡Hasta luego!');
+    window.location.href = 'tabs/tab1';
+  }
 
-
-    async showAlert(header: string, message: string) {
-      const alert = await this.alertController.create({
-        header,
-        message,
-        buttons: ['Aceptar'],
-      });
-      await alert.present();
-    }
-
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['Aceptar'],
+    });
+    await alert.present();
+  }
 }
